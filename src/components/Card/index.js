381649d4c7acccb1d9d36343fd270f4ed3ef0917
al3xs1lv4 <alessandro.gomes.free@gmail.com>
2023-02-15 18:35:57 -0300

    commit origin/main

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -8,12 +8,16 @@ const Card = ({ name, image, stats }) => {
                 <h3>{name}</h3>
             </div>
             <div className="info-pokemom">
-                <p>HP: {stats && stats[0] && stats[0].base_stat}</p>
-                <p>ATTACK: {stats && stats[1] && stats[1].base_stat}</p>
-                <p>DEFENSE: {stats && stats[2] && stats[2].base_stat}</p>
-                <p>SPECIAL ATTACK: {stats && stats[3] && stats[3].base_stat}</p>
-                <p>SPECIAL DEFENSE: {stats && stats[4] && stats[4].base_stat}</p>
-                <p>SPEED: {stats && stats[5] && stats[5].base_stat}</p>
+                <div>
+                    <p>hp: {stats && stats[0] && stats[0].base_stat}</p>
+                    <p>attack: {stats && stats[1] && stats[1].base_stat}</p>
+                    <p>defense: {stats && stats[2] && stats[2].base_stat}</p>
+                </div>
+                <div>
+                    <p>special attack: {stats && stats[3] && stats[3].base_stat}</p>
+                    <p>special defense: {stats && stats[4] && stats[4].base_stat}</p>
+                    <p>speed: {stats && stats[5] && stats[5].base_stat}</p>
+                </div>
             </div>
             <div></div>
         </div>
--- src/components/PokemonSearch/index.js
+++ src/components/PokemonSearch/index.js
@@ -1,11 +1,11 @@
-import axios from "axios";
-import { useState } from "react";
+import React, { useState } from 'react';
+import axios from 'axios';
 import Card from '../Card';
-import Search from '../Search'
+import Search from '../Search';
 import './PokemomSearch.css'
 
 const PokemomSearch = () => {
-    const [pokemonName, setPokemonName] = useState("");
+    const [pokemonName, setPokemonName] = useState('');
     const [pokemonData, setPokemonData] = useState({});
 
     const handleSubmit = (e) => {
@@ -20,9 +20,11 @@ const PokemomSearch = () => {
                 setPokemonData(res.data);
             });
     };
+    const getPokemonEvolution = () => {
+        axios.get(`https://pokeapi.co/api/v2/evolution-chain/${pokemonData.id}/`)
+    }
 
     return (
-
         <div className="container__content">
             <Search handleSubmit={handleSubmit} pokemonName={pokemonName} setPokemonName={setPokemonName} />
             <Card name={pokemonData.name} image={pokemonData.sprites && pokemonData.sprites.front_default} stats={pokemonData.stats} />
@@ -30,4 +32,4 @@ const PokemomSearch = () => {
     );
 };
 
-export default PokemomSearch
+export default PokemomSearch;
