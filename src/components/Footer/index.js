eac95186d1cb054a6087ee93c611c4e690e06784
al3xs1lv4 <alessandro.gomes.free@gmail.com>
2023-02-19 22:07:22 -0300

    Atualização do footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -3,12 +3,16 @@ import './Footer.css'
 const Footer = () => {
     return (
         <footer className='footer'>
-            <a target={'_blank'} href='https://allessandrogomes.github.io/portfolio/'><h4>Web Development by Alessandro Gomes</h4></a>      
-            <div>
-                <a target={'_blank'} href='https://github.com/allessandrogomes'><img src='./images/github-icon.svg' /></a>
-                <a target={'_blank'} href='https://www.linkedin.com/in/alessandro-da-silva-gomes-a82286240/'><img src='./images/linkedin-icon.svg'/></a>
+            <div className='text__footer'>
+                <a target={'_blank'} href='https://allessandrogomes.github.io/portfolio/'><h4>Web Development by Alessandro Gomes</h4></a>
             </div>
-            <a target={'_blank'} href='https://pokeapi.co'><h5>PokéApi</h5></a>
+            <div className='links__footer'>
+                <a target={'_blank'} href='https://github.com/allessandrogomes'><img src='./images/github-icon.svg' /></a>
+                <a target={'_blank'} href='https://www.linkedin.com/in/alessandro-da-silva-gomes-a82286240/'><img src='./images/linkedin-icon.svg' /></a>
+            </div>
+            <div className='pokeapi-link__footer'>
+                <a target={'_blank'} href='https://pokeapi.co'><h5>PokéApi</h5></a>
+            </div>
         </footer>
     )
 }
